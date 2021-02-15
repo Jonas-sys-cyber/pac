36436caa35585e67e6bc515aa53914e2efed1ba0
Luis Braun <69043370+luis-07@users.noreply.github.com>
2021-02-15 21:50:18 +0100

    add pac -S

--- main.py
+++ main.py
@@ -25,6 +25,17 @@ try:
 
     if args[1] == "-b" or args[1] == "--bash":
         install(args[2], args[3])
+    if args[1] == "-S":
+        os.system(f"git clone {args[2]}")
+        liste = str(args[2])
+        liste = liste.split("/")
+        #os.system(f"cd {liste[len(liste)-1]}")
+        f = open("exec.sh", "a")
+        f.write("#!/bin/bash \n")
+        f.write(f"python3 /usr/local/bin/{liste[len(liste)-1]}.py")
+        f.close()
+        install("exec.sh", args[3])
+        os.system(f"sudo mv {liste[len(liste)-1]}/main.py /usr/local/bin/{liste[len(liste)-1]}.py")
 
 except:
     pass
