2930a86deb9f1a3f7ecf7502267c5817ed670858
Luis Braun <69043370+luis-07@users.noreply.github.com>
2021-02-15 21:02:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-
 import os
 import sys
+
 args = []
 for i, arg in enumerate(sys.argv):
     args.append(arg)
-print(args[1])
-def install(directory=str,filename=str, commandname=str):
+
+def install(filename=str, commandname=str):
     os.system(f"chmod +x {filename}")
     os.system(f"sudo mv {filename} /usr/local/bin/{commandname}")
     print(f"sudo mv {filename} usr/local/bin/{commandname}")
 
-#install("home/luis/Downloads/", "command.sh", "testcommand")
-install("~/", "test.sh", "neo")
 
-if args[1] == 
+if args[1] == "-b" or args[1] == "--bash":
+    install(args[2], args[3])
